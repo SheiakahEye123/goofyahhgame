e64e7c2470c2789623d426e9ae99c4d87b839754
BuildTools <unconfigured@null.spigotmc.org>
2022-10-05 22:34:54 -0700

    alr so epic sytche bullet

--- src/scythe.java
+++ src/scythe.java
@@ -3,30 +3,48 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class scythe extends item{
-    long time1 = 0;
+    long time;
+    long time1;
     public scythe() {
         super();
         img = new ImageIcon("src/textures/scythe.png").getImage();
         name = "scythe";
-        time = System.nanoTime()/1000000000;
+        time = System.nanoTime();
     }
 
     public void use(boolean trigger, Graphics g, ArrayList<ArrayList<tile>> tilesOnScreen, WorldState worldstate) {
-        System.out.println(time + "/" + time1);
-        if (cooldown(0.5)) {
+        time1 = System.nanoTime();
+        if (true) {
             System.out.println("e");
             if (trigger) {
-                for (double b = 0; b <= 5; b++) {
-                    worldstate.bullets.add(new bullet(worldstate.Player.x - 1.25 + (b / 2), worldstate.Player.y - (b / 3), 12, 0.1, false, "bleed"));
+                bullet leadBullet = new bullet(worldstate.Player.x - 1.25, worldstate.Player.y, 12, 0.1, false, "bleed");
+
+                double targetX = worldstate.Player.x + 1.25;
+                double targetY = worldstate.Player.y - 1;
+                worldstate.bullets.add(leadBullet);
+                if (leadBullet.x > targetX) {
+                    leadBullet.accelerate(false,true,false,false, false,0.0000000000003);
+                    worldstate.bullets.add(new bullet(leadBullet.x, leadBullet.y, 12, 0.1, false, "bleed"));
                 }
+                if (leadBullet.x < targetX) {
+                    leadBullet.accelerate(false,false,false,true, false,0.0000000000003);
+                    worldstate.bullets.add(new bullet(leadBullet.x, leadBullet.y, 12, 0.1, false, "bleed"));
+                }
+                if (leadBullet.y < targetY) {
+                    leadBullet.accelerate(false,false,true,false, false,0.0000000000003);
+                    worldstate.bullets.add(new bullet(leadBullet.x, leadBullet.y, 12, 0.1, false, "bleed"));
+                }
+                if (leadBullet.y > targetY) {
+                    leadBullet.accelerate(true,false,false,false, false,0.0000000000003);
+                    worldstate.bullets.add(new bullet(leadBullet.x, leadBullet.y, 12, 0.1, false, "bleed"));
+                }
+
             }
             for (int b = 0; b < worldstate.bullets.size(); b++) {
-//                if (scytheslash.get(b).isCollided(scytheslash.get(b).x,scytheslash.get(b).y,tilesOnScreen,10,10)) {
-//                    scytheslash.remove(b);
-//                }
+                g.drawRect((int) ((worldstate.bullets.get(b).x * 64 + 960)), (int) ((worldstate.bullets.get(b).y) * 64 + 540),10,10);
             }
+            time = System.nanoTime();
         }
-        time1 = time;
     }
 
     public boolean cooldown(double s) {
