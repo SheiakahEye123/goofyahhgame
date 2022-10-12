297978d6e07b1a1bb7968b28a324fe4ed53ab3e6
BuildTools <unconfigured@null.spigotmc.org>
2022-10-11 21:52:53 -0700

    idk no more

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,19 +1,16 @@
+import kotlin.Pair;
+
 import java.awt.*;
 
 public class QuadTree {
     WorldState worldstate = new WorldState();
-    public void split(){
-        int maxBoundsX = worldstate.tiles.tileslist.get(0).size();
-        int maxBoundsY = worldstate.tiles.tileslist.size();
-        int minBoundsX = 0;
-        int minBoundsY = 0;
+    public Pair<Point,Point> split(int sMinBoundX, int sMinBoundY, int sMaxBoundX, int sMaxBoundY){
 
         Point size = new Point(maxBoundsX/4,maxBoundsY/4);
         if (size.x != 1 & size.y != 1) {
             int subRectX = maxBoundsX/2;
             int subRectY = maxBoundsY/2;
         }
-
-
+        return new Pair(new Point(sMinBoundX,sMinBoundY),new Point(sMaxBoundX, sMaxBoundY));
     }
 }
